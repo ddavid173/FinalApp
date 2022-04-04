a62d668605aa7ddf886e36bf6b2e7eb48397a5f6
MarcAbdallah <marcabdallah32@gmail.com>
2022-04-04 10:50:22 -0500

    trying to add gesture recognition

--- app/src/main/java/com/example/finalapp/SecondActivity.kt
+++ app/src/main/java/com/example/finalapp/SecondActivity.kt
@@ -1,15 +1,27 @@
 package com.example.finalapp
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.VelocityTracker
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import com.example.finalapp.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
 
     lateinit var backButton: Button
     private lateinit var binding: ActivitySecondBinding
+
+//    private var mVelocityTracker: VelocityTracker? = null
+
+    private var character = findViewById<ImageView>(R.id.character)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -21,8 +33,21 @@ class SecondActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             toMain()
         }
-
     }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val action = event.action
+//        if (action == MotionEvent.ACTION_DOWN) {
+//            tx = event.x
+//        }
+        if (action == MotionEvent.ACTION_MOVE){
+            val tx = event.x
+            character.x = tx
+        }
+        return true
+    }
+
+
 
     private fun toMain() {
         val intent = Intent(this,MainActivity::class.java).apply {}
